bd60e2bef05b1e39b3598b8729bc52de6e9486fa
mfahmifadhs <mfahmifadh2@gmail.com>
2024-06-13 16:02:13 +0700

    update table

--- resources/views/admin/pages/jadwal/detail.blade.php
+++ resources/views/admin/pages/jadwal/detail.blade.php
@@ -75,7 +75,7 @@
                 </div>
                 <div class="card-body">
                     <div class="table-responsive">
-                        <table id="table-history" class="table table-bordered text-xs text-center">
+                        <table id="table" class="table table-bordered text-xs text-center">
                             <thead>
                                 <tr>
                                     <th>No</th>
