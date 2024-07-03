f4ee5a3be4f44797c0a9b6dbc5bb24f58044fe4e
mfahmifadhs <mfahmifadh2@gmail.com>
2024-07-03 14:48:19 +0700

    update

--- resources/views/admin/pages/jadwal/detail.blade.php
+++ resources/views/admin/pages/jadwal/detail.blade.php
@@ -103,8 +103,8 @@
                                         @endif
                                     </td>
                                     <td class="text-left">{{ $loop->iteration }}. {{ $row->member->nama }}</td>
-                                    <td class="text-left">{{ $loop->nip_nik }}</td>
-                                    <td class="text-left">{{ $loop->tanggal_lahir }}</td>
+                                    <td class="text-left">{{ $row->nip_nik }}</td>
+                                    <td class="text-left">{{ $row->tanggal_lahir }}</td>
                                     <td class="text-left">{{ $row->member->instansi == 'pusat' ? $row->member->uker?->nama_unit_kerja : $row->member->nama_instansi }}</td>
                                     <td>
                                         @if($row->kehadiran == 'hadir')
