076ebc75e3b179c58cf4ffa42f896f5173bfaee3
mfahmifadhs <mfahmifadh2@gmail.com>
2024-07-12 15:52:54 +0700

    update

--- resources/views/admin/pages/konsul/show.blade.php
+++ resources/views/admin/pages/konsul/show.blade.php
@@ -50,7 +50,7 @@
                 </div>
             </div>
 
-            <div class="card">
+            <div class="card p-2">
                 <div class="card-header">
                     <label class="card-title text-sm mt-1">
                         <i class="fas fa-circle-check text-success"></i> Daftar Pasien
@@ -61,108 +61,108 @@
                         </button>
                     </div>
                 </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-borderless">
-                            <tbody>
-                                @foreach ($konsul as $row)
-                                <tr>
-                                    <td style="width: 10%;">
-                                        <h3 class="text-info"><b>{{ $loop->iteration }}</b></h3>
-                                    </td>
-                                    <td>
-                                        <a href="" data-toggle="modal" data-target="#modal-{{ $row->id_konsul }}">
-                                            <div class="card mt-2 p-2 text-dark">
-                                                <h6 class="text-xs">
-                                                    <span class="mr-1">
-                                                        @if ($row->test_sipgar == 1) <i class="fas fa-check-circle text-success"></i> Test Sipgar
-                                                        @else <i class="fas fa-times-circle text-danger"></i> Test Sipgar @endif
+                <div class="table-responsive">
+                    <table class="table table-borderless">
+                        <tbody>
+                            @foreach ($konsul as $row)
+                            <tr>
+                                <td style="width: 10%;">
+                                    <h3 class="text-info"><b>{{ $loop->iteration }}</b></h3>
+                                </td>
+                                <td>
+                                    <a href="" data-toggle="modal" data-target="#modal-{{ $row->id_konsul }}">
+                                        <div class="card mt-2 p-2 text-dark">
+                                            <h6 class="text-xs">
+                                                <span class="mr-1">
+                                                    @if ($row->test_sipgar == 1) <i class="fas fa-check-circle text-success"></i> Test Sipgar
+                                                    @else <i class="fas fa-times-circle text-danger"></i> Test Sipgar @endif
 
-                                                        @if ($row->test_fitness == 1) <i class="fas fa-check-circle text-success"></i> Test Fitness
-                                                        @else <i class="fas fa-times-circle text-danger"></i> Test Fitness @endif
+                                                    @if ($row->test_fitness == 1) <i class="fas fa-check-circle text-success"></i> Test Fitness
+                                                    @else <i class="fas fa-times-circle text-danger"></i> Test Fitness @endif
 
-                                                        @if ($row->konsultasi == 1) <i class="fas fa-check-circle text-success"></i> Konsultasi
-                                                        @else <i class="fas fa-times-circle text-danger"></i> Konsultasi @endif
-                                                    </span>
-                                                </h6>
-                                                <h6 class="text-xs pt-2">
-                                                    {{ Carbon\Carbon::parse($row->created_at)->isoFormat('DD MMMM Y || HH:mm') }} <br>
-                                                    {{ $row->member->nama }} <br>
-                                                    {{ $row->member->instansi == 'pusat' ? $row->member->uker->nama_unit_kerja : $row->member->nama_instansi }}
-                                                </h6>
-                                            </div>
-                                        </a>
+                                                    @if ($row->konsultasi == 1) <i class="fas fa-check-circle text-success"></i> Konsultasi
+                                                    @else <i class="fas fa-times-circle text-danger"></i> Konsultasi @endif
+                                                </span>
+                                            </h6>
+                                            <h6 class="text-xs pt-2">
+                                                {{ Carbon\Carbon::parse($row->created_at)->isoFormat('DD MMMM Y || HH:mm') }} <br>
+                                                {{ $row->member->nama }} <br>
+                                                {{ $row->member->instansi == 'pusat' ? $row->member->uker->nama_unit_kerja : $row->member->nama_instansi }}
+                                            </h6>
+                                        </div>
+                                    </a>
 
-                                        <div class="modal fade" id="modal-{{ $row->id_konsul }}" role="dialog">
-                                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                                <div class="modal-content">
-                                                    <form id="form" action="{{ route('konsul.update', $row->id_konsultasi) }}" method="GET">
-                                                        @csrf
-                                                        <div class="modal-header">
-                                                            <h5 class="modal-title text-md" id="modal-{{ $row->id_konsul }}">
-                                                                {{ $loop->iteration.' - '.$row->member->nama }} <br>
-                                                                <small style="margin-left: 13%;">
-                                                                    {{ $row->member->instansi == 'pusat' ? $row->member->uker->nama_unit_kerja : $row->member->nama_instansi }}
-                                                                </small>
-                                                            </h5>
-                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                <span aria-hidden="true">&times;</span>
-                                                            </button>
-                                                        </div>
-                                                        <div class="modal-body">
-                                                            <div class="row">
-                                                                <div class="col-md-4 col-4">1. Test Sipgar</div>
-                                                                <div class="col-md-8 col-8">:
-                                                                    <!-- Test Sipgar -->
-                                                                    <input id="true-sipgar" type="radio" name="test[1]" class="ml-2" value="1" {{ $row->test_sipgar == true ? 'checked' : '' }}>
-                                                                    <label for="true-sipgar"><i class="fa fa-check-circle text-xs text-success"></i> sudah</label>
+                                    <div class="modal fade" id="modal-{{ $row->id_konsul }}" role="dialog">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                                <form id="form" action="{{ route('konsul.update', $row->id_konsultasi) }}" method="GET">
+                                                    @csrf
+                                                    <div class="modal-header">
+                                                        <h5 class="modal-title text-md" id="modal-{{ $row->id_konsul }}">
+                                                            {{ $loop->iteration.' - '.$row->member->nama }} <br>
+                                                            <small style="margin-left: 13%;">
+                                                                {{ $row->member->instansi == 'pusat' ? $row->member->uker->nama_unit_kerja : $row->member->nama_instansi }}
+                                                            </small>
+                                                        </h5>
+                                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                            <span aria-hidden="true">&times;</span>
+                                                        </button>
+                                                    </div>
+                                                    <div class="modal-body">
+                                                        <div class="row">
+                                                            <div class="col-md-4 col-4">1. Test Sipgar</div>
+                                                            <div class="col-md-8 col-8">:
+                                                                <!-- Test Sipgar -->
+                                                                <input id="true-sipgar" type="radio" name="test[1]" class="ml-2" value="1" {{ $row->test_sipgar == true ? 'checked' : '' }}>
+                                                                <label for="true-sipgar"><i class="fa fa-check-circle text-xs text-success"></i> sudah</label>
 
-                                                                    <input id="false-sipgar" type="radio" name="test[1]" class="ml-2" value="0" {{ $row->test_sipgar == false ? 'checked' : '' }}>
-                                                                    <label for="false-sipgar"><i class="fa fa-times-circle text-xs text-danger"></i> belum</label>
-                                                                </div>
-
-
-                                                                <div class="col-md-4 col-4">2. Test Fitness</div>
-                                                                <div class="col-md-8 col-8">:
-                                                                    <!-- Test Fitness -->
-                                                                    <input id="true-fitness" type="radio" name="test[2]" class="ml-2" value="1" {{ $row->test_fitness == true ? 'checked' : '' }}>
-                                                                    <label for="true-fitness"><i class="fa fa-check-circle text-xs text-success"></i> sudah</label>
-
-                                                                    <input id="false-fitness" type="radio" name="test[2]" class="ml-2" value="0" {{ $row->test_fitness == false ? 'checked' : '' }}>
-                                                                    <label for="false-fitness"><i class="fa fa-times-circle text-xs text-danger"></i> belum</label>
-                                                                </div>
-
-                                                                <div class="col-md-12 col-12 mt-3">Konsultasi :</div>
-                                                                <div class="col-md-12 col-12 text-sm mt-3">
-                                                                    <label for="catatan_dokter">
-                                                                        Catatan Dokter <br>
-                                                                        <span class="text-danger" style="font-size: 10px;">
-                                                                            Catatan bersifat rahasia, dan tidak akan diberitahukan kepada pasien
-                                                                        </span>
-                                                                    </label>
-                                                                    <textarea name="catatan_dokter" class="form-control" id="catatan_dokter" rows="5">{{ $row->catatan_dokter }}</textarea>
-                                                                </div>
-                                                                <div class="col-md-12 col-12 text-sm mt-3">
-                                                                    <label for="catatan_pasien">Catatan Pasien</label>
-                                                                    <textarea name="catatan_pasien" class="form-control" id="catatan_pasien" rows="5">{{ $row->catatan_pasien }}</textarea>
-                                                                </div>
+                                                                <input id="false-sipgar" type="radio" name="test[1]" class="ml-2" value="0" {{ $row->test_sipgar == false ? 'checked' : '' }}>
+                                                                <label for="false-sipgar"><i class="fa fa-times-circle text-xs text-danger"></i> belum</label>
                                                             </div>
+
+
+                                                            <div class="col-md-4 col-4">2. Test Fitness</div>
+                                                            <div class="col-md-8 col-8">:
+                                                                <!-- Test Fitness -->
+                                                                <input id="true-fitness" type="radio" name="test[2]" class="ml-2" value="1" {{ $row->test_fitness == true ? 'checked' : '' }}>
+                                                                <label for="true-fitness"><i class="fa fa-check-circle text-xs text-success"></i> sudah</label>
+
+                                                                <input id="false-fitness" type="radio" name="test[2]" class="ml-2" value="0" {{ $row->test_fitness == false ? 'checked' : '' }}>
+                                                                <label for="false-fitness"><i class="fa fa-times-circle text-xs text-danger"></i> belum</label>
+                                                            </div>
+
+                                                            @if ($row->test_sipgar == 1 && $row->test_fitness == 1)
+                                                            <div class="col-md-12 col-12 mt-3">Konsultasi :</div>
+                                                            <div class="col-md-12 col-12 text-sm mt-3">
+                                                                <label for="catatan_dokter">
+                                                                    Catatan Dokter <br>
+                                                                    <span class="text-danger" style="font-size: 10px;">
+                                                                        Catatan bersifat rahasia, dan tidak akan diberitahukan kepada pasien
+                                                                    </span>
+                                                                </label>
+                                                                <textarea name="catatan_dokter" class="form-control" id="catatan_dokter" rows="5">{{ $row->catatan_dokter }}</textarea>
+                                                            </div>
+                                                            <div class="col-md-12 col-12 text-sm mt-3">
+                                                                <label for="catatan_pasien">Catatan Pasien</label>
+                                                                <textarea name="catatan_pasien" class="form-control" id="catatan_pasien" rows="5">{{ $row->catatan_pasien }}</textarea>
+                                                            </div>
+                                                            @endif
                                                         </div>
-                                                        <div class="modal-footer">
-                                                            <button type="submit" class="btn btn-primary btn-sm btn-info" onclick="confirmSubmit(event)">
-                                                                <i class="fa fa-save"></i> Simpan
-                                                            </button>
-                                                        </div>
-                                                    </form>
-                                                </div>
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-primary btn-sm btn-info" onclick="confirmSubmit(event)">
+                                                            <i class="fa fa-save"></i> Simpan
+                                                        </button>
+                                                    </div>
+                                                </form>
                                             </div>
                                         </div>
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
